fec536773e37518d239979e971e7506bfd611043
lillie <lillie@Lillies-MBP.lan>
2021-05-21 23:37:00 -0700

    updated html and js

---
+++ script.js
@@ -0,0 +1,35 @@
+// Assign variables
+var viewHighScores = document.querySelector("#viewHighScores");
+var timerEl = document.querySelector("#timer");
+var questBlanks = document.querySelector(".questions-blanks");
+var container = document.querySelector("#container");
+var  firstPage = document.querySelector("#firstPage");
+var startGameBtn = document.querySelector("#btn-start");
+var quiz = document.querySelector("#quiz");
+var question = document.querySelector("#question");
+var choiceA = document.getElementById("A");
+var choiceB = document.getElementById("B");
+var choiceC = document.getElementById("C");
+var choiceD = document.getElementById("D");
+
+
+
+var runQuestion = 0;
+var chosenQuest = [];
+var timer;
+var timerCount; 
+
+
+console.log(viewHighScores);
+console.log(timerEl);
+console.log(questBlanks);
+console.log(container);
+console.log(firstPage);
+console.log(startGameBtn);
+console.log(quiz);
+console.log(question);
+console.log(choiceA);
+console.log(choiceB);
+console.log(choiceC);
+console.log(choiceD);
+
