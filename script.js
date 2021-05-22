6a034f655f8f92517d099e9bb8447c97baf4e5a8
lillie <lillie@Lillies-MBP.lan>
2021-05-21 23:43:47 -0700

    added f. renderQuestion and renderContinue in js

--- script.js
+++ script.js
@@ -84,3 +84,63 @@ var questions = [{
 }]; 
 console.log(questions[0]);
 
+
+const lastQuestion = questions.length -1;
+console.log(lastQuestion);
+
+// The startGame function is called when the start button is clicked
+// Work!!!
+function startGame() {
+    timerCount = 75;
+    startGameBtn.disabled = true;
+    // when click start quiz, first page will be gone
+    firstPage.style.display = "none";  
+    //the quiz part won't appear untill the start btn is clicked 
+    quiz.style.display = "block";
+    startTimer()
+    renderQuestion()
+}
+
+// The setTimer function starts and decrement the time by 10 sec. if incorrect
+// when click startGameBtn > timer run from 75 sec to 0 + question appear & run
+// Time countdown >> Work!!!!
+function startTimer(){
+    // set timer
+    timer = setInterval(() => {
+        timerCount--;
+        timerEl.textContent = timerCount;
+        if (timerCount >= 0) {
+        if (timerCount === 0){
+            clearInterval(timer);
+        }
+        }
+    }, 1000);
+}
+
+// create the function to make the first question run
+function renderQuestion(){
+    var quest = questions[runQuestion];
+
+    question.textContent = quest.question;
+    choiceA.textContent = quest.choiceA;
+    choiceB.textContent = quest.choiceB;
+    choiceC.textContent = quest.choiceC;
+    choiceD.textContent = quest.choiceD;
+
+}
+
+function renderContinue(){
+    for(let questIndex = 0; questIndex <= lastQuestion; questIndex++)}
+
+
+ 
+
+
+renderQuestion();
+
+
+
+
+// Attach event listener to start button to call startGame function on click
+startGameBtn.addEventListener("click", startGame); 
+    
